285f896597e33c6370f9a02dd5af93bac7e08d7a
Will Daly <rwilliamdaly@gmail.com>
2014-05-20 10:16:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+take-out-2
+===============
+a model of a food-ordering service, as an educational example of authentication.
+- Node.js
+- Express
+- MongoDB
+- Traceur
+- Jade
+- Less
---
+++ app/static/js/es6/sockets.es6.js
@@ -0,0 +1,18 @@
+/* global io */
+
+(function(){
+  'use strict';
+
+  $(document).ready(initialize);
+
+  var socket;
+
+  function initialize(){
+    initializeSocketIo();
+  }
+
+  function initializeSocketIo(){
+    socket = io.connect('/app');
+  }
+
+})();
